cfdf99b6b9034699eb7eb520373cea560633c0b7
Ibrahim San <ibrahimattiasan@gmail.com>
2022-08-15 16:20:24 +0400

    15% of the project completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        "custome-grey-blue": "#B9B6D3",
+        "custome-dark-blue": "#3E3C49",
+        "custome-blue": "#6055A5", 
+        "custome-red": "#FF7A7A",
+        "custome-green":"#38CC8C"
+      }
+    },
   },
   plugins: [],
 }
